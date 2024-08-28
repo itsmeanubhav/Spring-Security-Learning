5a16de58c64b49187011cf4dba1c4b533063a2d8
“itsmeanubhav” <imanubhav074@gmail.com>
2024-08-28 12:50:45 +0530

    JDBC SCHEMA CREATION DIRECTLY FROM SPRING SECURITY

---
+++ src/main/java/com/springSecurity/jdbcAuthentication/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.springSecurity.jdbcAuthentication;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.withDefaultSchema()	//Spring Boot creates Default Schema(As of now H2 database)
+			.withUser(
+					User.withUsername("user")
+					.password("pass")
+					.roles("USER")
+					)
+			.withUser(
+					User.withUsername("admin")
+					.password("pass")
+					.roles("ADMIN")
+					);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests() //Mentions we need to authorize all requests
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+		}
+	
+		/*
+		 * @Override protected void configure(HttpSecurity http) throws Exception {
+		 * 
+		 * http.authorizeRequests() .antMatchers("/admin").hasRole("ADMIN")
+		 * .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+		 * .antMatchers("/").permitAll() .and().formLogin(); }
+		 */
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+}
